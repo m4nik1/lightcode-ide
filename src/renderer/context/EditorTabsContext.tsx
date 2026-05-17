5bfdbff7ea9cf0473b6e07dc8bd99853018fca02
m4nik1 <maniksoomro@hotmail.com>
2026-05-16 20:32:00 -0400

    Changed spacing for editorTabs

--- src/renderer/context/EditorTabsContext.tsx
+++ src/renderer/context/EditorTabsContext.tsx
@@ -2,13 +2,13 @@ import { createContext, ReactNode, useContext, useState, useEffect, React } from
 import { EditorTab } from "../types/EditorTab";
 
 type EditorTabsContext = {
-    activeFilePath: string,
-    setActivePath: React.Dispatch<React.SetStateAction<string>>,
-    tabs: EditorTab[],
-    setTabs: React.Dispatch<React.SetStateAction<EditorTab[]>>,
-    openFile: (filePath : string) => void;
-    activeFolder : string
-    openFolder: () => void;
+  activeFilePath: string,
+  setActivePath: React.Dispatch<React.SetStateAction<string>>,
+  tabs: EditorTab[],
+  setTabs: React.Dispatch<React.SetStateAction<EditorTab[]>>,
+  openFile: (filePath: string) => void;
+  activeFolder: string
+  openFolder: () => void;
 }
 
 const tabContext = createContext<EditorTabsContext | undefined>(undefined)
@@ -18,77 +18,77 @@ function getFileName(filePath: string) {
   return filePath.split(/[\\/]/).pop() ?? filePath;
 }
 
-export function EditorTabsProvider({ children } : { children : ReactNode }) {
-    const [activeFilePath, setActivePath] = useState('');
-    const [tabs, setTabs] = useState<EditorTab[] | []>([]);
-    const [activeFolder, setFolder] = useState('');
+export function EditorTabsProvider({ children }: { children: ReactNode }) {
+  const [activeFilePath, setActivePath] = useState('');
+  const [tabs, setTabs] = useState<EditorTab[] | []>([]);
+  const [activeFolder, setFolder] = useState('');
 
-    let nextTabID : number = tabs.length;
+  let nextTabID: number = tabs.length;
 
 
-    // Opens file and adds to the tabs
-    function openFile(filePath : string) {
-        setTabs((tabs : EditorTab[]) => {
-            const ifOpen = tabs.some((tabs) => tabs.filePath === filePath)
+  // Opens file and adds to the tabs
+  function openFile(filePath: string) {
+    setTabs((tabs: EditorTab[]) => {
+      const ifOpen = tabs.some((tabs) => tabs.filePath === filePath)
 
-            if(ifOpen) {
-                return tabs;
-            }
+      if (ifOpen) {
+        return tabs;
+      }
 
-            nextTabID += 1;
+      nextTabID += 1;
 
-            return [
-                ...tabs,
-                {
-                    id: nextTabID,
-                    filename: getFileName(filePath),
-                    filePath,
-                    isModified: false,
-                }
-            ]
-        });
-    }
-
-    async function openFolder() {
-        const folder = await window.electronAPI.openFolder()
-        const folderPath = folder.filePaths[0]
-
-        if (!folderPath) {
-          return
+      return [
+        ...tabs,
+        {
+          id: nextTabID,
+          filename: getFileName(filePath),
+          filePath,
+          isModified: false,
         }
+      ]
+    });
+  }
 
-        console.log("Selected: ", folderPath)
+  async function openFolder() {
+    const folder = await window.electronAPI.openFolder()
+    const folderPath = folder.filePaths[0]
 
-        setFolder(folderPath);
+    if (!folderPath) {
+      return
     }
 
-    useEffect(() => {
-        console.log("I am currently on: ", activeFilePath);
-    }, [activeFilePath])
+    console.log("Selected: ", folderPath)
 
-    return(
-        <tabContext.Provider
-            value={{
-                activeFilePath,
-                setActivePath,
-                tabs,
-                setTabs,
-                openFile,
-                openFolder,
-                activeFolder
-            }}
-        >
-            {children}
-        </tabContext.Provider>
-    )
+    setFolder(folderPath);
+  }
+
+  useEffect(() => {
+    console.log("I am currently on: ", activeFilePath);
+  }, [activeFilePath])
+
+  return (
+    <tabContext.Provider
+      value={{
+        activeFilePath,
+        setActivePath,
+        tabs,
+        setTabs,
+        openFile,
+        openFolder,
+        activeFolder
+      }}
+    >
+      {children}
+    </tabContext.Provider>
+  )
 }
 
 export function useEditorTabs() {
-    const context = useContext(tabContext);
+  const context = useContext(tabContext);
 
-      if (!context) {
-        throw new Error("useEditorTabs must be used within an EditorTabsProvider");
-    }
+  if (!context) {
+    throw new Error("useEditorTabs must be used within an EditorTabsProvider");
+  }
 
-    return context;
+  return context;
 }
