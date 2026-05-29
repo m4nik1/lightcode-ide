247766b7498f5ddc2a566280ecd0941203c62279
m4nik1 <maniksoomro@hotmail.com>
2026-05-29 01:16:06 -0400

    Took out useless import

--- src/renderer/context/EditorTabsContext.tsx
+++ src/renderer/context/EditorTabsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState, useEffect, React } from "react";
+import { createContext, ReactNode, useContext, useState, useEffect } from "react";
 import { EditorTab } from "../types/EditorTab";
 
 type EditorTabsContext = {
