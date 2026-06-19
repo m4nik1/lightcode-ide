d46f425f62c3fb396780b1f1559ae75a35ab9fc8
Manik <m.soomro@cuescript.tv>
2026-06-19 12:00:21 -0400

    Added send query func in server

--- apps/server/lightThread.ts
+++ apps/server/lightThread.ts
@@ -1,7 +1,9 @@
-import { type Codex } from "@openai/codex-sdk";
+import { type Codex, type Thread } from "@openai/codex-sdk";
 
 
 export default class lightThread {
+  codexInstance: Codex;
+  thread: null | Thread;
   constructor(codexInstance: Codex) {
     this.codexInstance = codexInstance
     this.thread = null;
@@ -10,6 +12,12 @@ export default class lightThread {
   createThread() {
     console.log("Creating thread")
 
+    this.thread = this.codexInstance.startThread();
+  }
 
+  async sendQuery(query: string) {
+    const result = await this.thread.run(query)
+
+    return result
   }
 }
