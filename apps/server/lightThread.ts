afd6c7434be9fc6a7f2266ae37c9e9f400c85431
m4nik1 <maniksoomro@hotmail.com>
2026-07-28 00:10:22 -0400

    Finished migration of lightThread

--- apps/server/lightThread.ts
+++ apps/server/lightThread.ts
@@ -1,8 +1,7 @@
-import { type ModelReasoningEffort, type Codex, type Thread } from "@openai/codex-sdk";
+import { type ModelReasoningEffort } from "@openai/codex-sdk";
 import {
   CodexAppServerClient,
   ThreadStartResponse,
-  type InitializeThreadResponse,
 } from '@lightcode/codex-protocol'
 
 export default class lightThread {
@@ -17,11 +16,7 @@ export default class lightThread {
     this.turnId = null;
   }
 
-  async createThread(model: string, thinking: ModelReasoningEffort, path : string) {
-    console.log("Creating thread, model: ", model, " thinking: ", thinking)
-
-    // this.thread = this.codexInstance.startThread({model, modelReasoningEffort: thinking, workingDirectory: path});
-
+  async createThread(path : string) {
     // The approval policy is about the full access dropdown that is shown
     this.thread = await this.codexInstance.startThread({
       cwd: path,
@@ -48,13 +43,15 @@ export default class lightThread {
     })
   }
 
-  async *sendQueryStream(query: string) {
+  async *sendQueryStream(model: string, thinking: ModelReasoningEffort, query: string) {
     if (!this.thread || !this.id) {
       throw new Error("Thread has not been created")
     }
 
     for await (const event of this.codexInstance.streamTurn({
       threadId: this.id,
+      model: model,
+      effort: thinking,
       input: [
         {
           type: "text",
