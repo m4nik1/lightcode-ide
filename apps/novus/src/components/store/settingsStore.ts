9480529a880f8d131a6c4ae9e52539504452e1d8
m4nik1 <maniksoomro@gmail.com>
2026-08-23 23:29:32 -0400

    Added persistence

--- apps/novus/src/components/store/settingsStore.ts
+++ apps/novus/src/components/store/settingsStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 const initialSettings = {
   colorMode: 'dark'
@@ -9,13 +10,21 @@ interface settingsState {
   setColorMode: () => void;
 }
 
-const useSettings = create<settingsState>((set) => ({
-  colorMode: 'dark',
-  setColorMode: (mode) => {
-    set({ mode })
-    setColorMode(mode)
-  }
-}));
+const useSettings = create<settingsState>()(
+  persist(
+    (set, get) => ({
+      colorMode: 'dark',
+      setColorMode: (mode) => {
+        set({ mode })
+        setColorMode(mode)
+      }
+    }),
+    {
+      name: 'settings-storage',
+      storage: createJSONStorage(() => sessionStorage)
+    }
+  )
+)
 
 function setColorMode(colorMode : string) {
   console.log("Setting color mode: ", colorMode);
