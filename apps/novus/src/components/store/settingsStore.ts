56474d9879e0404e3c741ea52e2e860a2a9483d1
m4nik1 <maniksoomro@hotmail.com>
2026-08-23 21:07:04 -0400

    Added method for settings store

--- apps/novus/src/components/store/settingsStore.ts
+++ apps/novus/src/components/store/settingsStore.ts
@@ -1,6 +1,19 @@
 import { create } from 'zustand'
 
-const useSettings = create((set) => {
-  colorMode: 'dark',
-  
-})
+const initialSettings = {
+  colorMode: 'dark'
+}
+
+interface settingsState {
+  colorMode: () => void;
+}
+
+const useSettings = create<settingsState>((set) => ({
+  colorMode: () => set((state) => setColorMode(state.colorMode)),
+}));
+
+function setColorMode(colorMode : string) {
+  console.log("Setting color mode: ", colorMode);
+}
+
+export default useSettings;
