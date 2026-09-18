26e9eb7332bd54452d5d59034c0501acb240d845
raj <maniksoomro@hotmail.com>
2026-09-17 21:12:09 -0400

    Finished useProjects refactor

--- apps/novus/src/context/useProjects.tsx
+++ apps/novus/src/context/useProjects.tsx
@@ -1,3 +1,43 @@
 /*
     This hook is here to load the projects/threads in the sidebar
 */
+
+
+export function projectProvider({ children } : { children: ReactNode }) {
+  const projectsQuery = useQuery({
+    queryKey: ["projects"],
+    queryFn: () => trpcClient.getProjects.query(),
+  });
+
+  async function createProject() {
+    const projectFolder = await window.electronAPI.openFolder();
+    const projectFolderPath = projectFolder.filePaths[0];
+
+    if (!projectFolderPath) {
+      return;
+    }
+
+    const folderName = projectFolderPath.split("/")?.at(-1);
+
+    if (!folderName) {
+      return;
+    }
+
+    await trpcClient.addProject.mutate({
+      projectName: folderName,
+      path: projectFolderPath,
+    });
+
+    await projectsQuery.refetch();
+  }
+
+  return (
+    <projectsContext.provider
+      value={{
+        projectsQuery,
+        createProject
+      }}
+    >
+    </projectsContext.provider>
+  )
+}
